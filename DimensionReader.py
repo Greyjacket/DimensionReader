1963fe35c0f761bd3167614ecf36596d46cc192c
Greyjacket <asch716@gmail.com>
2018-03-09 04:37:12 -0500

    Alpha Test

--- DimensionReader.py
+++ DimensionReader.py
@@ -1,20 +1,34 @@
 from PIL import Image
+import pandas as pd
 import glob, os
 import datetime
 
-size = 8192, 8192
+if not (os.path.isdir('Images')):
+    print ("Please create an Images directory.")
+    exit()
+
+dfcols = ['image_name', 'image_width', 'image_height']
+df_image = pd.DataFrame(columns=dfcols)
 
 before = datetime.datetime.now()
 
-for infile in glob.glob("*.jpg"):
+for infile in glob.glob("Images/*.jpg"):
     file, ext = os.path.splitext(infile)
+    df_image_temp = pd.DataFrame(columns=dfcols)
     im = Image.open(infile)
-    im.thumbnail(size)
-    im.save("thumbnails/" + file + ".thumbnail", "JPEG")
+    image_name = im.filename
+    image_width = (im.size[0])
+    image_height = (im.size[1])
+
+    df_image_temp = pd.DataFrame({'image_name':[image_name], 'image_width':[image_width], 'image_height':[image_height]})
+    df_image = df_image.append(df_image_temp)
+
+df_image.to_csv("image_dimensions.csv", encoding='utf-8', columns=dfcols)
 
 after = datetime.datetime.now()
 
 delta = after - before
 ms = int(delta.total_seconds() * 1000)
 
-print "Finished in " + str(ms) + " milliseconds"
+print ("Finished in " + str(ms) + " milliseconds")
+print ("File written to image_dimensions.csv")
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DimensionReader
 Scans images and retrieves their height and width in pixels. Returns a CSV,
+
+Installation:
+
+pip install Pillow
+pip install Pandas
+
+Create an Images directory below the script.
+
+CSV will be output to the current directory.
